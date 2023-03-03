3f874bd62f33987dea5b722866cde46f84f21411
Hannah Porta <hporta2@lsu.edu>
2023-03-02 20:47:10 -0600

    yes

--- source/Ann_Model.py
+++ source/Ann_Model.py
@@ -11,10 +11,10 @@ class ModelBuilder2(DataPreprocessing):
     def __init__(self, *args, **kwargs):
         super(ModelBuilder2, self).__init__(*args, **kwargs)
 
-    def dt(self, X_train, X_test, y_train, y_test,maxi, learnrate):
+    def dt(self, X_train, X_test, y_train, y_test):
         #adding things to change
 
-        ANN_classifier = MLPClassifier(hidden_layer_sizes=(5,5), max_iter=maxi,learning_rate_init=learnrate)
+        ANN_classifier = MLPClassifier(max_iter=20000, hidden_layer_sizes=(4,4),learning_rate_init=.01)
 
         #Train the model
         ANN_classifier.fit(X_train, y_train)
